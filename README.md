4f5588150923977079be81afcd6b5b83219da241
TX2 <30351272@qq.com>
2022-05-15 15:46:53 +0800

    can get face.data and face_map.txt

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # face_feature
 当前版本是用运行generate一个节点生成face.data
 输入的图片是只截取了人脸的部分。
-读取图片依赖一个txt文件存储图片名。
+人脸照片命名格式为：数字_人名。eg:123_司机1
+读取照片需要给出照片文件夹路径参数。
+默认存储路径：
+std::string face_data_path = "/home/nvidia/Documents/face_data/";   //文件夹下存储face.data，为人脸特征库
+std::string map_path = "/home/nvidia/Documents/face_map/";  //文件夹下存储face_map.txt，为id映射表
--- src/generateFeature.cpp
+++ src/generateFeature.cpp
@@ -8,10 +8,11 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <string.h>
 
 #include <queue>
 #include <map>
-#include <string.h>
+#include <string>
 #include <vector>
 
 #include "./pfld/pfld.h"
@@ -23,14 +24,16 @@
 using std::cout;
 using std::endl;
 using std::map;
-using std::string;
 using std::ifstream;
 
 // string _path = "/home/nvidia/wdq/ros_vision/src/face_feature/src/data/";     //存.txt文件的路径
-string txt_path;
-string cap_path = "/home/nvidia/Pictures/face_cap/";    //待识别特征的人脸图片的路径
-string cap_pic_name;
-string face_data_filename = "/home/nvidia/Documents/face_data/face.data";
+
+const std::string suffix = ".jpg";
+const std::string separator = "_";
+// string cap_path = "/home/nvidia/Pictures/face_cap/";    //待识别特征的人脸图片的路径
+// string cap_pic_name;
+std::string face_data_path = "/home/nvidia/Documents/face_data/";
+std::string map_path = "/home/nvidia/Documents/face_map/";
 
 ros::Publisher DriverInfoPub;
 
@@ -47,6 +50,35 @@ inline void printVector(float *tmpFaceFeature){ //用于辅助测试特征是否
         cout << tmpFaceFeature[i] << " ";
     }
     cout << endl;
+}
+
+//a字符串减去b字符串，返回值存在result字符串，从尾部截去，ge:"123.jpg"-".jpg"='123'
+inline void StringSub(const std::string &a, const std::string &b, std::string &result)
+{
+    int pos = a.find(b);
+    if(pos != std::string::npos)
+    {
+        result = a.substr(0, pos);
+    }
+    else
+    {
+        cout << "string_b is not a substr of string_a." << endl;
+    }
+}
+
+//拆分人名和id
+inline void getIDName(const std::string &a, const std::string &separator, int &id, std::string &name)
+{
+    int pos = a.find(separator);
+    if(pos != std::string::npos)
+    {
+        id = std::stoi(a.substr(0, pos).c_str());
+        name = (a.substr(pos)).erase(0,separator.size());  //必须是非常量字符串
+    }
+    else
+    {
+        cout << "can  not find separator." << endl;
+    }
 }
 
 void imageCallback(const face_feature::FacePicMsg::ConstPtr& msg)
@@ -142,40 +174,112 @@ int main(int argc, char **argv)
     // cv::destroyWindow("view2");    //窗口
 
     //1.2直接读取本地的人脸照片
-    txt_path = argv[1]; //第二个参数开始
-    cout  << "argv[1]:  " << argv[1] << endl;
+    map<int, std::string> id_name_map;
+    std::string picture_path;
 
-    // std::vector<cv::String> picture_names;
-    // cv::glob(picture_path, picture_names);
-    const std::string txt = "face_cap_name.txt";
-    const std::string suffix = ".jpg";
-    unsigned int suffix_len = suffix.length();
-    ifstream infile;
-    infile.open(txt_path + txt);   //将文件流对象与文件连接起来
-    if(infile.is_open())
+    //路径
+    if(argc < 2)
     {
-            cout << "face_pic_name.txt opened successfully." << endl;
+        //std::cout << " 请传入3个地址参数，分别为存储照片路径，存储id映射表的路径，存储face.data文件的路径。"  << std::endl;
+        std::cout << " 请至少传入1个地址参数：存储照片路径。"  << std::endl;
+        return -2;
     }
-    //assert(infile.is_open());   //若失败,则输出错误消息,并终止程序
-    string line;                            //每次读取一行
-    cv::Mat img;                        //存储读到的本地照片
-    while(getline(infile, line))
+    switch (argc)
     {
-        cap_pic_name = line.substr(0, line.length() - suffix_len);
-        num = atoi(cap_pic_name.c_str());
-        cap_pic_name = cap_pic_name + ".jpg";
-        img = cv::imread(cap_path + cap_pic_name, 1); 
-        memset(tmpFaceFeature, 0, sizeof(tmpFaceFeature));
-        //printVector(tmpFaceFeature);
-        ArcFace::GetFaceFeature(img, tmpFaceFeature);
-        memcpy((gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION)), tmpFaceFeature, (ArcFace::FACE_FEATURE_DIMENSION) * sizeof(float));
-        // //test 
-        // printVector(gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION));
-        cout << "read " << cap_pic_name << " successfully." << endl;
+    case 2:
+    {
+        picture_path = argv[1]; //第二个参数开始
+        break;
+    }
+    case 3:
+    {
+        picture_path = argv[1]; 
+        map_path = argv[2];        
+        break;
+    }
+    case 4:
+    {
+        picture_path = argv[1]; 
+        map_path = argv[2];       
+        face_data_path = argv[3];          
+        break;
+    }
+    default:
+    {
+        cout << "wrong: too much parametor." << endl;
+        break;
+    }
     }
 
-    //写到本地
-    std::ofstream outFStream(face_data_filename.c_str(), std::ios::binary);
+
+    //判断输入地址末尾必须为/
+    if(*(picture_path.rbegin())!= '/' || *(map_path.rbegin())!= '/'|| *(face_data_path.rbegin())!= '/' )
+    {    
+        // cout << *(picture_path.rbegin()) << endl;
+        cout << "The path must end with '/', please input a new path." << endl;
+        return -1;
+    }
+    cout  << "picture path: "  << picture_path<< endl;
+    cout << "map path: " << map_path<< endl;
+    cout << "face data  path: " << face_data_path << endl;
+    std::vector<cv::String> picture_names; 
+    cv::glob(picture_path, picture_names);
+    std::string name;    //存单个人名;
+    int id;                 //照片编号
+
+    //read pictures
+
+    for(size_t i = 0; i < picture_names.size(); ++i)
+    {
+        // std::cout<<picture_names[i]<<std::endl;
+        //照片正常读取
+        cv::Mat src = cv::imread(picture_names[i]);
+        if(!src.data)
+        {
+            std::cerr << "Problem loading image!!!" << std::endl;            
+        }
+
+        name = picture_names[i];
+        name.erase(0, picture_path.size());     //去掉名字中的路径
+        StringSub(name, suffix, name);
+        getIDName(name, separator, id, name);
+        // cout << "No: " << id << "   Name: " << name << endl;
+        
+        if(id >= ArcFace::GALLARY_NUM)
+        {
+            cout << "id: " << id << "is out of range." << endl;
+        }
+        else
+        {
+             if(id_name_map.find(id) != id_name_map.end())
+            {
+                cout << "insert wrong. the id already exists." << endl;
+            }
+            else
+            {
+                id_name_map[id] = name;
+                // cout << id << ": " << id_name_map[id] << endl;
+                //add to gallaryData
+                memset(tmpFaceFeature, 0, sizeof(tmpFaceFeature));
+                //printVector(tmpFaceFeature);
+                ArcFace::GetFaceFeature(src, tmpFaceFeature);
+                memcpy((gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION)), tmpFaceFeature, (ArcFace::FACE_FEATURE_DIMENSION) * sizeof(float));
+                // //test 
+                // printVector(gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION));
+            }
+        }
+    }
+
+    //写map到txt
+    std::ofstream ous(map_path + "face_map.txt");
+    for(map<int,std::string>::iterator iter = id_name_map.begin(); iter != id_name_map.end(); ++iter){
+        // cout<<"key:"<<iter->first<<" value:"<<iter->second<<endl;
+        ous << iter->first << " "<< iter->second << endl;
+    }
+
+    //写gallaryData到本地face.data
+    face_data_path = face_data_path + "face.data";
+    std::ofstream outFStream(face_data_path.c_str(), std::ios::binary);
     if(!outFStream){
         std::cout << "Failed to open face data file." << std::endl;
     }
@@ -184,6 +288,46 @@ int main(int argc, char **argv)
     }
     outFStream.write((char *) &gallaryData, sizeof(gallaryData));
     outFStream.close();
+
+    // std::vector<cv::String> picture_names;
+    // cv::glob(picture_path, picture_names);
+    // const std::string txt = "face_cap_name.txt";
+    // const std::string suffix = ".jpg";
+    // unsigned int suffix_len = suffix.length();
+    // ifstream infile;
+    // infile.open(txt_path + txt);   //将文件流对象与文件连接起来
+    // if(infile.is_open())
+    // {
+    //         cout << "face_pic_name.txt opened successfully." << endl;
+    // }
+    //assert(infile.is_open());   //若失败,则输出错误消息,并终止程序
+    // string line;                            //每次读取一行
+    // cv::Mat img;                        //存储读到的本地照片
+    // while(getline(infile, line))
+    // {
+    //     cap_pic_name = line.substr(0, line.length() - suffix_len);
+    //     num = atoi(cap_pic_name.c_str());
+    //     cap_pic_name = cap_pic_name + ".jpg";
+    //     img = cv::imread(cap_path + cap_pic_name, 1); 
+    //     memset(tmpFaceFeature, 0, sizeof(tmpFaceFeature));
+    //     //printVector(tmpFaceFeature);
+    //     ArcFace::GetFaceFeature(img, tmpFaceFeature);
+    //     memcpy((gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION)), tmpFaceFeature, (ArcFace::FACE_FEATURE_DIMENSION) * sizeof(float));
+    //     // //test 
+    //     // printVector(gallaryData+num*(ArcFace::FACE_FEATURE_DIMENSION));
+    //     cout << "read " << cap_pic_name << " successfully." << endl;
+    // }
+
+    // //写到本地
+    // std::ofstream outFStream(face_data_filename.c_str(), std::ios::binary);
+    // if(!outFStream){
+    //     std::cout << "Failed to open face data file." << std::endl;
+    // }
+    // else{
+    //     std::cout<< "Face data file opened successfully." << std::endl;
+    // }
+    // outFStream.write((char *) &gallaryData, sizeof(gallaryData));
+    // outFStream.close();
 
     ArcFace::ReleasePFLDEngine();       //一样的名字
     return 0;
